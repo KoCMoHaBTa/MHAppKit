fed93ee77fb39bbaefc0dc98b65bdcd1583844d7
Milen Halachev <milenski88@gmail.com>
2016-03-03 23:59:19 +0200

    Initial Commit

---
+++ MHAppKit/MHAppKit.h
@@ -0,0 +1,19 @@
+//
+//  MHAppKit.h
+//  MHAppKit
+//
+//  Created by Milen Halachev on 3/3/16.
+//  Copyright © 2016 Milen Halachev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MHAppKit.
+FOUNDATION_EXPORT double MHAppKitVersionNumber;
+
+//! Project version string for MHAppKit.
+FOUNDATION_EXPORT const unsigned char MHAppKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MHAppKit/PublicHeader.h>
+
+
