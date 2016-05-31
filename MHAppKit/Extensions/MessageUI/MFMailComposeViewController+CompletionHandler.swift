412b879a81fad1840dd71308b570eebd12133407
Milen Yordanov Halachev <milenski88@gmail.com>
2016-05-31 16:27:17 +0300

    - added MFMailComposeViewController+CompletionHandler extension

---
+++ MHAppKit/Extensions/MessageUI/MFMailComposeViewController+CompletionHandler.swift
@@ -0,0 +1,52 @@
+//
+//  MFMailComposeViewController+CompletionHandler.swift
+//  MHAppKit
+//
+//  Created by Milen Halachev on 5/31/16.
+//  Copyright © 2016 Milen Halachev. All rights reserved.
+//
+
+import Foundation
+import MessageUI
+
+extension MFMailComposeViewController: MFMailComposeViewControllerDelegate, UINavigationControllerDelegate {
+    
+    public typealias CompletionHandler = (controller: MFMailComposeViewController, result: MFMailComposeResult, error: NSError?) -> Void
+    
+    private static var completionHandlerKey = ""
+    
+    public var completionHandler: CompletionHandler? {
+        
+        get {
+            
+            return (objc_getAssociatedObject(self, &MFMailComposeViewController.completionHandlerKey) as? CompletionHandlerWrapper)?.handler
+        }
+        
+        set {
+            
+            self.delegate = self
+            
+            objc_setAssociatedObject(self, &MFMailComposeViewController.completionHandlerKey, CompletionHandlerWrapper(newValue), .OBJC_ASSOCIATION_RETAIN_NONATOMIC)
+        }
+    }
+    
+    public func mailComposeController(controller: MFMailComposeViewController, didFinishWithResult result: MFMailComposeResult, error: NSError?) {
+        
+        self.completionHandler?(controller: controller, result: result, error: error)
+    }
+}
+
+extension MFMailComposeViewController {
+    
+    private class CompletionHandlerWrapper {
+        
+        let handler: MFMailComposeViewController.CompletionHandler
+        
+        init?(_ handler: MFMailComposeViewController.CompletionHandler?) {
+            
+            guard let handler = handler else { return nil }
+            
+            self.handler = handler
+        }
+    }
+}
