cf88d11493de8ac144f953097e247549716bff12
Milen Halachev <milenski88@gmail.com>
2017-07-26 11:01:42 +0300

    - added UIControlGroup + tests

---
+++ MHAppKit/UIControlGroup.swift
@@ -0,0 +1,231 @@
+//
+//  UIControlGroup.swift
+//  MHAppKit
+//
+//  Created by Milen Halachev on 7/25/17.
+//  Copyright © 2017 Milen Halachev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+open class UIControlGroup: NSObject {
+    
+    public enum StateAction: Int {
+        
+        case none = 0
+        case enabled        //change the enbled value
+        case hidden         //change the hidden value
+        case highlighted    //change the highlited value
+    }
+    
+    ///The controls that forms the group
+    @IBOutlet open var controls: [UIControl] = [] {
+        
+        didSet {
+            
+            let controls = self.controls.sorted { (c1, c2) -> Bool in
+                
+                return c1.tag > c2.tag
+            }
+            
+            for control in controls {
+                
+                control.addTarget(self, action: #selector(type(of: self).controlAction(_:)), for: .touchUpInside)
+            }
+            
+            self.controls = controls
+        }
+    }
+    
+    ///Controls whenver multiple selection is allowed. Default to false
+    @IBInspectable open var allowMultipleSelection: Bool = false
+    
+    ///Controls whenever no selection is allowed
+    @IBInspectable open var allowNoneSelection: Bool = false
+    
+    ///Assign to a control which action will deselect all controls in the group
+    @IBOutlet open var deselectAllControl: UIControl? {
+        
+        didSet {
+            
+            self.deselectAllControl?.addTarget(self, action: #selector(type(of: self).deselectAllControls), for: .touchUpInside)
+        }
+    }
+    
+    ///The state action assigned to all deselected controls
+    @IBInspectable open var deselectAllControlStateAction: StateAction = .none
+    
+    ///Assign to a control which action will select all controls in the group
+    @IBOutlet open var selectAllControl: UIControl? {
+        
+        didSet {
+            
+            self.selectAllControl?.addTarget(self, action: #selector(type(of: self).selectAllControls), for: .touchUpInside)
+        }
+    }
+    
+    ///The state action assigned to all selected controls
+    @IBInspectable open var selectAllControlStateAction: StateAction = .none
+    
+    //MARK: - Callbacks
+    
+    var didSelectControlHandler: ((UIControl) -> Void)?
+    var didDeselectControlHandler: ((UIControl) -> Void)?
+    var didChangeControlState: ((UIControl, StateAction) -> Void)?
+    
+    //MARK: - Behaviour
+    
+    ///Toggle the selection state of the control at the given index
+    open func toggleControl(at index: Int) {
+        
+        self.controlAction(self.controls[index])
+    }
+    
+    ///Deselect all controls. This works only if `allowNoneSelection` is `true`
+    @IBAction open func deselectAllControls() {
+        
+        if self.allowNoneSelection {
+            
+            for control in self.selectedControls {
+                
+                self.controlAction(control)
+            }
+        }
+    }
+    
+    ///Select all controls. This only works if `allowMultipleSelection` is `true`
+    @IBAction open func selectAllControls() {
+        
+        if self.allowMultipleSelection {
+            
+            for control in self.deselectedControls {
+                
+                self.controlAction(control)
+            }
+        }
+    }
+    
+    //MARK: - State
+    
+    ///The selected controls from the group
+    open var selectedControls: [UIControl] {
+        
+        return self.controls.filter({ (control) -> Bool in
+            
+            return control.isSelected == true
+        })
+    }
+    
+    ///The indexes of the selected controls from the group
+    open var selectedControlIndexes: [Int] {
+        
+        return self.selectedControls.map({ self.controls.index(of: $0)! })
+    }
+    
+    ///The deselected controls from the group
+    open var deselectedControls: [UIControl] {
+        
+        return self.controls.filter({ (control) -> Bool in
+            
+            return control.isSelected == false
+        })
+    }
+    
+    ///The indexes of the deselected controls from the group
+    open var deselectedControlIndexes: [Int] {
+        
+        return self.deselectedControls.map({ self.controls.index(of: $0)! })
+    }
+    
+    //MARK: - Private
+    
+    @IBAction private func controlAction(_ sender: UIControl) {
+        
+        if !self.allowNoneSelection && self.selectedControls.count == 1 && sender.isSelected {
+            
+            return
+        }
+        
+        sender.isSelected = !sender.isSelected
+        
+        if !self.allowMultipleSelection {
+            
+            var controlToDeselect: UIControl?
+            for control in self.controls {
+                
+                if control.isSelected && !control.isEqual(sender) {
+                    
+                    controlToDeselect = control
+                    break
+                }
+            }
+            
+            controlToDeselect?.isSelected = false
+            
+            if let controlToDeselect = controlToDeselect {
+                
+                self.didDeselectControlHandler?(controlToDeselect)
+            }
+        }
+        
+        self.updateStateActionsWithSelection(sender.isSelected)
+        
+        sender.isSelected ? self.didSelectControlHandler?(sender) : self.didDeselectControlHandler?(sender)
+    }
+    
+    private func updateStateActionsWithSelection(_ isSelected: Bool) {
+        
+        let selectedControlsCount = self.selectedControls.count
+        let controlsCount = self.controls.count
+        
+        //none are selected
+        if selectedControlsCount == 0
+        {
+            self.update(control: self.deselectAllControl, with: self.deselectAllControlStateAction)
+        }
+        
+        //all are selected
+        if selectedControlsCount == controlsCount
+        {
+            self.update(control: self.selectAllControl, with: self.selectAllControlStateAction)
+        }
+        
+        //any first selected - from none to one
+        if selectedControlsCount == 1 && isSelected
+        {
+            self.update(control: self.deselectAllControl, with: self.deselectAllControlStateAction)
+        }
+        
+        //any first deselected - from none to one
+        if selectedControlsCount == controlsCount - 1 && !isSelected
+        {
+            self.update(control: self.selectAllControl, with: self.selectAllControlStateAction)
+        }
+    }
+    
+    private func update(control: UIControl?, with stateAction: StateAction) {
+        
+        guard let control = control else {
+            
+            return
+        }
+        
+        switch stateAction
+        {
+            case .enabled:
+                control.isEnabled = !control.isEnabled
+                
+            case .hidden:
+                control.isHidden = !control.isHidden
+                
+            case .highlighted:
+                control.isHighlighted = !control.isHighlighted
+                
+            case .none:
+                break
+        }
+        
+        self.didChangeControlState?(control, stateAction)
+    }
+}
---
+++ MHAppKitTests/UIControlGroupTests.swift
@@ -0,0 +1,218 @@
+//
+//  UIControlRadioGroupTests.swift
+//  MHAppKit
+//
+//  Created by Milen Halachev on 7/25/17.
+//  Copyright © 2017 Milen Halachev. All rights reserved.
+//
+
+import Foundation
+import XCTest
+@testable import MHAppKit
+
+class UIControlGroupTests: XCTestCase {
+    
+    func testUIControlRadioGroup_MSNO_NSNO() {
+        
+        let group = UIControlGroup()
+        group.controls = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
+        group.selectAllControl = UIButton()
+        group.deselectAllControl = UIButton()
+        
+        XCTAssertEqual(group.controls.count, 5)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.allowMultipleSelection = false
+        group.allowNoneSelection = false
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 4])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [4])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [4])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.selectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.deselectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+    }
+    
+    func testUIControlRadioGroup_MSYES_NSNO() {
+        
+        let group = UIControlGroup()
+        group.controls = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
+        group.selectAllControl = UIButton()
+        group.deselectAllControl = UIButton()
+        
+        XCTAssertEqual(group.controls.count, 5)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.allowMultipleSelection = true
+        group.allowNoneSelection = false
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [1, 3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 4])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [1, 3, 4])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [1, 3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.selectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [0, 1, 2, 3, 4])
+        XCTAssertEqual(group.deselectedControlIndexes, [])
+        
+        group.deselectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [0, 1, 2, 3, 4])
+        XCTAssertEqual(group.deselectedControlIndexes, [])
+    }
+    
+    func testUIControlRadioGroup_MSNO_NSYES() {
+        
+        let group = UIControlGroup()
+        group.controls = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
+        group.selectAllControl = UIButton()
+        group.deselectAllControl = UIButton()
+        
+        XCTAssertEqual(group.controls.count, 5)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.allowMultipleSelection = false
+        group.allowNoneSelection = true
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 4])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [4])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.selectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.deselectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+    }
+    
+    func testUIControlRadioGroup_MSYES_NSYES() {
+        
+        let group = UIControlGroup()
+        group.controls = [UIButton(), UIButton(), UIButton(), UIButton(), UIButton()]
+        group.selectAllControl = UIButton()
+        group.deselectAllControl = UIButton()
+        
+        XCTAssertEqual(group.controls.count, 5)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.allowMultipleSelection = true
+        group.allowNoneSelection = true
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [1, 3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 4])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [1, 3, 4])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2])
+        
+        group.toggleControl(at: 4)
+        XCTAssertEqual(group.selectedControlIndexes, [1, 3])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 4])
+        
+        group.toggleControl(at: 3)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+        
+        group.toggleControl(at: 1)
+        XCTAssertEqual(group.selectedControlIndexes, [1])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 2, 3, 4])
+        
+        group.selectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [0, 1, 2, 3, 4])
+        XCTAssertEqual(group.deselectedControlIndexes, [])
+        
+        group.deselectAllControls()
+        XCTAssertEqual(group.selectedControlIndexes, [])
+        XCTAssertEqual(group.deselectedControlIndexes, [0, 1, 2, 3, 4])
+    }
+}
