d1e4954ebaa1ac8c1213fa4cfd6e6c65475a17bb
Milen Halachev <milenski88@gmail.com>
2017-12-19 12:03:43 +0200

    - BlockOperation - added convenience initialization for async block execution based on semaphore

---
+++ MHAppKit/Extensions/Foundation/BlockOperation+Async.swift
@@ -0,0 +1,27 @@
+//
+//  BlockOperation+Async.swift
+//  MHAppKit
+//
+//  Created by Milen Halachev on 19.12.17.
+//  Copyright © 2017 Milen Halachev. All rights reserved.
+//
+
+import Foundation
+
+extension BlockOperation {
+    
+    public convenience init(async: @escaping (_ completion: () -> Void) -> Void) {
+        
+        self.init {
+            
+            let semaphore = DispatchSemaphore(value: 0)
+            
+            async({
+                
+                semaphore.signal()
+            })
+            
+            semaphore.wait()
+        }
+    }
+}
