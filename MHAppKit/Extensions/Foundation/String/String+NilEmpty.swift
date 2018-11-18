05d26b16eea10538844140a7e14315809d1672f9
Milen Halachev <milenski88@gmail.com>
2018-11-18 23:11:43 +0200

    - fixed access modifiers

--- MHAppKit/Extensions/Foundation/String/String+NilEmpty.swift
+++ MHAppKit/Extensions/Foundation/String/String+NilEmpty.swift
@@ -11,19 +11,19 @@ import Foundation
 extension String {
     
     ///returns nil if the receiver is empty
-    var nonEmpty: String? {
+    public var nonEmpty: String? {
         
         return self.isEmpty ? nil : self
     }
     
     ///Trims whitespaces and newlines of the receiver and returns the trimmed receiver or nil if empty
-    var nonEmptyTrimmed: String? {
+    public var nonEmptyTrimmed: String? {
         
         return self.trimmingWhitespacesAndNewlines.nonEmpty
     }
     
     ///Trims whitespaces and newlines of the receiver and returns the result
-    var trimmingWhitespacesAndNewlines: String {
+    public var trimmingWhitespacesAndNewlines: String {
         
         return self.trimmingCharacters(in: .whitespacesAndNewlines)
     }
