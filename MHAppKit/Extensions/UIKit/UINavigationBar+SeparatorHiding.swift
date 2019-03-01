aa2bfdd0a88e72f19c850153090cbbc7ccee5dbe
Milen Halachev <milenski88@gmail.com>
2019-03-01 10:22:35 +0200

    - added UINavigationBar+SeparatorHiding extension - set version to 1.2.0

---
+++ MHAppKit/Extensions/UIKit/UINavigationBar+SeparatorHiding.swift
@@ -0,0 +1,36 @@
+//
+//  UINavigationBar+SeparatorHiding.swift
+//  MHAppKit
+//
+//  Created by Milen Halachev on 1.03.19.
+//  Copyright © 2019 Milen Halachev. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UINavigationBar {
+    
+    public static var hiddenSeparatorImage = UIImage()
+    
+    @IBInspectable open var isSeparatorHidden: Bool {
+        
+        get { return self.shadowImage === type(of: self).hiddenSeparatorImage && self.backIndicatorImage === type(of: self).hiddenSeparatorImage }
+        set { newValue ? self.hideSeparator() : self.showSeparator() }
+    }
+    
+    open func showSeparator() {
+        
+        //restore the navigation bar separator
+        self.shadowImage = nil
+        self.backIndicatorImage = nil
+    }
+    
+    open func hideSeparator() {
+        
+        //remove the navigation bar separator
+        self.shadowImage = type(of: self).hiddenSeparatorImage
+        self.backIndicatorImage = type(of: self).hiddenSeparatorImage
+    }
+}
+
