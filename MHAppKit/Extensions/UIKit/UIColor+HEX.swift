736505d7f260308f235fe4d0a8ce6eaf1746334f
Milen Halachev <milenski88@gmail.com>
2019-06-12 15:37:37 +0300

    - UIColor - added support for RGBA and ARGB converting

--- MHAppKit/Extensions/UIKit/UIColor+HEX.swift
+++ MHAppKit/Extensions/UIKit/UIColor+HEX.swift
@@ -34,6 +34,16 @@ extension UIColor {
         self.init(red: red, green: green, blue: blue, alpha: alpha)
     }
     
+    public convenience init(R: UInt8, G: UInt8, B: UInt8, A: UInt8) {
+        
+        let red = CGFloat(R)/CGFloat(UInt8.max)
+        let green = CGFloat(G)/CGFloat(UInt8.max)
+        let blue = CGFloat(B)/CGFloat(UInt8.max)
+        let alpha = CGFloat(A)/CGFloat(UInt8.max)
+        
+        self.init(red: red, green: green, blue: blue, alpha: alpha)
+    }
+    
     /**
      
      Creates an instance of the receiver with 24bit RGB value and alpha
@@ -59,32 +69,116 @@ extension UIColor {
     
     /**
      
-     Creates an instance of the receiver with HEX String value and alpha
+     Creates an instance of the receiver with 32bit RGBA value
      
-     - parameter HEX: The HEX value from "000000" to "FFFFFF"
+     - parameter RGBA: The RGBA value from 0 to 4294967295
+     
+     */
+    
+    public convenience init(RGBA: UInt32) {
+        
+        let R = UInt8((RGBA & 0xFF000000) >> 24)
+        let G = UInt8((RGBA & 0x00FF0000) >> 16)
+        let B = UInt8((RGBA & 0x0000FF00) >> 8)
+        let A = UInt8((RGBA & 0x000000FF) >> 0)
+        
+        self.init(R: R, G: G, B: B, A: A)
+    }
+    
+    /**
+     
+     Creates an instance of the receiver with 32bit ARGB value
+     
+     - parameter ARGB: The ARGB value from 0 to 4294967295
+     
+     */
+    
+    public convenience init(ARGB: UInt32) {
+        
+        let A = UInt8((ARGB & 0xFF000000) >> 24)
+        let R = UInt8((ARGB & 0x00FF0000) >> 16)
+        let G = UInt8((ARGB & 0x0000FF00) >> 8)
+        let B = UInt8((ARGB & 0x000000FF) >> 0)
+        
+        self.init(R: R, G: G, B: B, A: A)
+    }
+    
+    /**
+     
+     Creates an instance of the receiver with RGB HEX String value and alpha
+     
+     - parameter HEX: The RGB HEX value from "000000" to "FFFFFF"
      - parameter alpha: The alpha component value from 0 to 1. Default to 1
      
      - returns: An instance of the receiver or nil if the HEX parameter is invalid.
      
      */
     
+    @available(*, deprecated, message: "Use init?(RGB:alpha:)")
     public convenience init?(HEX: String, alpha: CGFloat = 1) {
         
-        var HEX = HEX
-        var RGB: UInt32 = 0
+        self.init(RGB: HEX, alpha: alpha)
+    }
+    
+    /**
+     
+     Creates an instance of the receiver with HEX String value and alpha
+     
+     - parameter RGB: The RGB HEX value from "000000" to "FFFFFF"
+     - parameter alpha: The alpha component value from 0 to 1. Default to 1
+     
+     - returns: An instance of the receiver or nil if the RGB parameter is invalid.
+     
+     */
+    
+    public convenience init?(RGB HEX: String, alpha: CGFloat = 1) {
         
-        //remove hash tag from HEX string
-        if HEX.first == "#" {
-            
-            HEX.remove(at: HEX.startIndex)
-        }
-        
-        guard Scanner(string: HEX).scanHexInt32(&RGB) else {
+        guard let RGB = UInt32(convertingFromHEX: HEX) else {
             
             return nil
         }
         
         self.init(RGB: RGB, alpha: alpha)
+    }
+    
+    /**
+     
+     Creates an instance of the receiver with HEX String value and alpha
+     
+     - parameter RGBA: The RGBA HEX value from "00000000" to "FFFFFFFF"
+     
+     - returns: An instance of the receiver or nil if the RGBA parameter is invalid.
+     
+     */
+    
+    public convenience init?(RGBA HEX: String) {
+        
+        guard let RGBA = UInt32(convertingFromHEX: HEX) else {
+            
+            return nil
+        }
+        
+        self.init(RGBA: RGBA)
+    }
+    
+    /**
+     
+     Creates an instance of the receiver with HEX String value and alpha
+     
+     - parameter ARGB: The ARGB HEX value from "00000000" to "FFFFFFFF"
+     
+     - returns: An instance of the receiver or nil if the ARGB parameter is invalid.
+     
+     */
+
+    public convenience init?(ARGB HEX: String) {
+        
+        guard let ARGB = UInt32(convertingFromHEX: HEX) else {
+            
+            return nil
+        }
+        
+        self.init(ARGB: ARGB)
     }
 }
 
@@ -93,8 +187,10 @@ extension UIColor {
     ///Represents a color space type
     public enum ColorSpace {
         
-        case automatic
+        case automatic //rgb or hsb or white
         case rgb
+        case rgba //rgb that inclues the alpha as the last component.
+        case argb //rgb that inclues the alpha as the first component.
         case hsb
         case white
     }
@@ -132,6 +228,34 @@ extension UIColor {
                 }
             
                 return ([R, G, B], A)
+            
+            case .rgba:
+                
+                var R: CGFloat = 0
+                var G: CGFloat = 0
+                var B: CGFloat = 0
+                var A: CGFloat = 0
+                
+                guard self.getRed(&R, green: &G, blue: &B, alpha: &A) else {
+                    
+                    return nil
+                }
+                
+                return ([R, G, B, A], A)
+            
+            case .argb:
+                
+                var R: CGFloat = 0
+                var G: CGFloat = 0
+                var B: CGFloat = 0
+                var A: CGFloat = 0
+                
+                guard self.getRed(&R, green: &G, blue: &B, alpha: &A) else {
+                    
+                    return nil
+                }
+                
+                return ([A, R, G, B], A)
             
             case .hsb:
             
@@ -242,8 +366,49 @@ extension UIColor {
     
     
     ///The HEX representation of the receiver
+    @available(*, deprecated, message: "Use RGB instead.")
     public var HEX: String? {
         
         return self.HEX()?.HEX
+    }
+    
+    ///The RGB HEX representation of the receiver
+    public var RGB: String? {
+        
+        return self.HEX(of: .rgb)?.HEX
+    }
+    
+    ///The RGBA HEX representation of the receiver
+    public var RGBA: String? {
+        
+        return self.HEX(of: .rgba)?.HEX
+    }
+    
+    ///The ARGB HEX representation of the receiver
+    public var ARGB: String? {
+        
+        return self.HEX(of: .argb)?.HEX
+    }
+}
+
+extension UInt32 {
+    
+    fileprivate init?(convertingFromHEX HEX: String) {
+        
+        var HEX = HEX
+        var result: UInt32 = 0
+        
+        //remove hash tag from HEX string
+        if HEX.first == "#" {
+            
+            HEX.remove(at: HEX.startIndex)
+        }
+        
+        guard Scanner(string: HEX).scanHexInt32(&result) else {
+            
+            return nil
+        }
+        
+        self = result
     }
 }
--- MHAppKitTests/UIColorTests.swift
+++ MHAppKitTests/UIColorTests.swift
@@ -27,26 +27,56 @@ class UIColorTests: XCTestCase {
         XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(RGB: 0xFFFFFF, alpha: 1))
         XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(RGB: 0x000000))
         
+        //RGBA Number
+        XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1), UIColor(RGBA: 0xFF0000FF))
+        XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.4), UIColor(RGBA: 0x00FF0066))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(RGBA: 0x0000FF00))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(RGBA: 0xFFFFFFFF))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(RGBA: 0x000000FF))
+        
+        //ARGB Number
+        XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1), UIColor(ARGB: 0xFFFF0000))
+        XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.4), UIColor(ARGB: 0x6600FF00))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(ARGB: 0x000000FF))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(ARGB: 0xFFFFFFFF))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(ARGB: 0xFF000000))
+        
         //RGB HEX String
-        XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1), UIColor(HEX: "FF0000", alpha: 1))
-        XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.5), UIColor(HEX: "00FF00", alpha: 0.5))
-        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(HEX: "#0000FF", alpha: 0))
-        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(HEX: "#FFFFFF", alpha: 1))
-        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(HEX: "000000"))
+        XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1), UIColor(RGB: "FF0000", alpha: 1))
+        XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.5), UIColor(RGB: "00FF00", alpha: 0.5))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(RGB: "#0000FF", alpha: 0))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(RGB: "#FFFFFF", alpha: 1))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(RGB: "000000"))
+        
+        //RGBA HEX String
+        XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1), UIColor(RGBA: "FF0000FF"))
+        XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.4), UIColor(RGBA: "00FF0066"))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(RGBA: "#0000FF00"))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(RGBA: "#FFFFFFFF"))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(RGBA: "000000FF"))
+        
+        //ARGB HEX String
+        XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1), UIColor(ARGB: "FFFF0000"))
+        XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.4), UIColor(ARGB: "6600FF00"))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(ARGB: "#000000FF"))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(ARGB: "#FFFFFFFF"))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 0, alpha: 1), UIColor(ARGB: "FF000000"))
         
         XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(RGB: 16777215))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(RGBA: UInt32.max))
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 1), UIColor(ARGB: 4294967295))
         
         //lower case HEX string
-        XCTAssertEqual(UIColor(R: 200, G: 199, B: 204), UIColor(HEX: "c8c7cc"))
+        XCTAssertEqual(UIColor(R: 200, G: 199, B: 204), UIColor(RGB: "c8c7cc"))
         
         //test hash tagged hex strings
-        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(HEX: "#0000FF", alpha: 0))
-        XCTAssertEqual(UIColor(R: 200, G: 199, B: 204), UIColor(HEX: "#c8c7cc"))
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0), UIColor(RGB: "#0000FF", alpha: 0))
+        XCTAssertEqual(UIColor(R: 200, G: 199, B: 204), UIColor(RGB: "#c8c7cc"))
     }
     
     func testHEX() {
         
-        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 0.34).HEX, "#FFFFFF")
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 0.34).RGB, "#FFFFFF")
         XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 0.34).HEX()?.alpha, 0.34)
         
         XCTAssertEqual(UIColor(red: 1, green: 0, blue: 0, alpha: 1).HEX(includeHashtag: true)?.HEX, "#FF0000")
@@ -55,13 +85,16 @@ class UIColorTests: XCTestCase {
         XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.5).HEX(includeHashtag: false)?.HEX, "00FF00")
         XCTAssertEqual(UIColor(red: 0, green: 1, blue: 0, alpha: 0.5).HEX()?.alpha, 0.5)
         
-        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0).HEX, "#0000FF")
+        XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0).RGB, "#0000FF")
         XCTAssertEqual(UIColor(red: 0, green: 0, blue: 1, alpha: 0).HEX()?.alpha, 0)
+        
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 0.4).RGBA, "#FFFFFF66")
+        XCTAssertEqual(UIColor(red: 1, green: 1, blue: 1, alpha: 0.4).ARGB, "#66FFFFFF")
     }
     
     func testErrors() {
         
-        XCTAssertNil(UIColor(HEX: "this should fail"))
+        XCTAssertNil(UIColor(RGB: "this should fail"))
         XCTAssertNil(UIColor(RGB: 16777216))
     }
 }
