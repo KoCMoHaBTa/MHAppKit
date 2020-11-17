f001d5e37e1b3ea95ac3391807e50e5ab916e116
Milen Halachev <milenski88@gmail.com>
2020-11-17 15:56:47 +0200

    - added UserDefaultsStored property wrapper

---
+++ MHAppKit/PropertyWrappers/UserDefaultsStored.swift
@@ -0,0 +1,90 @@
+//
+//  UserDefaultsStored.swift
+//  MHAppKit
+//
+//  Created by Milen Halachev on 17.11.20.
+//  Copyright © 2020 Milen Halachev. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct UserDefaultsStored<T> {
+    
+    public let initialValue: T
+    public let key: String
+    public let userDefaults: UserDefaults
+    
+    public var wrappedValue: T {
+        
+        get {
+            
+            return self.userDefaults.value(forKey: self.key) as! T
+        }
+        
+        set {
+            
+            if self.isNil(newValue){
+                
+                self.userDefaults.removeObject(forKey: self.key)
+            }
+            else {
+                
+                self.userDefaults.set(newValue, forKey: self.key)
+            }
+            
+            self.userDefaults.synchronize()
+        }
+    }
+    
+    public var projectedValue: Self {
+        
+        return self
+    }
+    
+    private var exists: Bool {
+        
+        return self.userDefaults.value(forKey: self.key) != nil
+    }
+    
+    private var isCorrectType: Bool {
+        
+        return self.userDefaults.value(forKey: self.key) is T
+    }
+    
+    public init(wrappedValue initialValue: T, key: String, userDefaults: UserDefaults) {
+        
+        self.initialValue = initialValue
+        self.key = key
+        self.userDefaults = userDefaults
+        
+        if !self.exists || !self.isCorrectType {
+            
+            self.wrappedValue = initialValue
+        }
+    }
+    
+    public init(wrappedValue initialValue: T, key: String) {
+        
+        self.init(wrappedValue: initialValue, key: key, userDefaults: .standard)
+    }
+    
+    public func observeChanges(handler: @escaping (T) -> Void) -> Any {
+        
+        return self.userDefaults.observeChanges(forKey: self.key) { (value: T?) in
+            
+            handler(self.wrappedValue)
+        }
+    }
+    
+    private func isOptional<T>(_ value: T) -> Bool {
+        
+        return value is OptionalTypeVerifiable
+    }
+    
+    private func isNil<T>(_ value: T) -> Bool {
+        
+        return (value as? OptionalTypeVerifiable)?.isNil == true
+    }
+}
+
--- MHAppKitTests/UserDefaultsTests.swift
+++ MHAppKitTests/UserDefaultsTests.swift
@@ -69,4 +69,156 @@ class UserDefaultsTests: XCTestCase {
         _ = observer
         observer = nil
     }
+    
+    func testPropertyWraper() {
+        
+        class Mock {
+            
+            struct Keys {
+                
+                static let k1: String = .uuid
+                static let k2: String = .uuid
+                static let k3: String = .uuid
+            }
+            
+            @UserDefaultsStored(key: Keys.k1) var v1: Int = 5
+            @UserDefaultsStored(key: Keys.k2) var v2: Int? = 10
+            @UserDefaultsStored(key: Keys.k3) var v3: Int? = nil
+        }
+        
+        let mock = Mock()
+        
+        var lastV1: Int? = nil
+        var o1: Any? = mock.$v1.observeChanges { (value) in
+            
+            lastV1 = value
+        }
+        
+        var lastV2: Int? = nil
+        var o2: Any? = mock.$v2.observeChanges { (value) in
+            
+            lastV2 = value
+        }
+        
+        var lastV3: Int? = nil
+        var o3: Any? = mock.$v3.observeChanges { (value) in
+            
+            lastV3 = value
+        }
+        
+        XCTAssertEqual(mock.v1, 5)
+        XCTAssertEqual(mock.v2, 10)
+        XCTAssertNil(mock.v3)
+        
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k1), 5)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k2), 10)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k3), 0)
+        
+        XCTAssertNil(lastV1)
+        XCTAssertNil(lastV2)
+        XCTAssertNil(lastV3)
+        
+        mock.v1 = 7
+        mock.v2 = nil
+        mock.v3 = 20
+        
+        XCTAssertEqual(mock.v1, 7)
+        XCTAssertNil(mock.v2)
+        XCTAssertEqual(mock.v3, 20)
+        
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k1), 7)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k2), 0)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k3), 20)
+        
+        XCTAssertEqual(lastV1, 7)
+        XCTAssertNil(lastV2)
+        XCTAssertEqual(lastV3, 20)
+        
+        
+        _ = o1
+        _ = o2
+        _ = o3
+        o1 = nil
+        o2 = nil
+        o3 = nil
+    }
+    
+    func testPropertyWraperWithWrongValueTypes() {
+
+        class Mock {
+            
+            struct Keys {
+                
+                static let k1: String = .uuid
+                static let k2: String = .uuid
+                static let k3: String = .uuid
+            }
+            
+            @UserDefaultsStored(key: Keys.k1) var v1: Int = 5
+            @UserDefaultsStored(key: Keys.k2) var v2: Int? = 10
+            @UserDefaultsStored(key: Keys.k3) var v3: Int? = nil
+        }
+        
+        //set initial values of wrong type
+        UserDefaults.standard.set("asd", forKey: Mock.Keys.k1)
+        UserDefaults.standard.set("wer", forKey: Mock.Keys.k2)
+        UserDefaults.standard.set("sfgd", forKey: Mock.Keys.k3)
+        UserDefaults.standard.synchronize()
+        
+        let mock = Mock()
+        
+        var lastV1: Int? = nil
+        var o1: Any? = mock.$v1.observeChanges { (value) in
+            
+            lastV1 = value
+        }
+        
+        var lastV2: Int? = nil
+        var o2: Any? = mock.$v2.observeChanges { (value) in
+            
+            lastV2 = value
+        }
+        
+        var lastV3: Int? = nil
+        var o3: Any? = mock.$v3.observeChanges { (value) in
+            
+            lastV3 = value
+        }
+        
+        XCTAssertEqual(mock.v1, 5)
+        XCTAssertEqual(mock.v2, 10)
+        XCTAssertNil(mock.v3)
+        
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k1), 5)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k2), 10)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k3), 0)
+        
+        XCTAssertNil(lastV1)
+        XCTAssertNil(lastV2)
+        XCTAssertNil(lastV3)
+        
+        mock.v1 = 7
+        mock.v2 = nil
+        mock.v3 = 20
+        
+        XCTAssertEqual(mock.v1, 7)
+        XCTAssertNil(mock.v2)
+        XCTAssertEqual(mock.v3, 20)
+        
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k1), 7)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k2), 0)
+        XCTAssertEqual(UserDefaults.standard.integer(forKey: Mock.Keys.k3), 20)
+        
+        XCTAssertEqual(lastV1, 7)
+        XCTAssertNil(lastV2)
+        XCTAssertEqual(lastV3, 20)
+        
+        
+        _ = o1
+        _ = o2
+        _ = o3
+        o1 = nil
+        o2 = nil
+        o3 = nil
+    }
 }
